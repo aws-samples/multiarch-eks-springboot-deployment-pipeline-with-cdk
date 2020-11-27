3f4bfd44d770c53e9ab5152ddedbfb7b600df594
lazydragon <lazy.dragon@hotmail.com>
2020-11-27 08:25:02 +0000

    fix env name

--- cdk/app.py
+++ cdk/app.py
@@ -8,8 +8,8 @@ from aws_cdk import core
 from pipeline.pipeline_stack import PipelineStack
 from backend.backend_stack import BackendStack
 
-env = core.Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"],
-                       region=os.environ["CDK_DEFAULT_REGION"])
+env = core.Environment(account=os.environ["CDK_DEPLOY_ACCOUNT"],
+                       region=os.environ["CDK_DEPLOY_REGION"])
 
 app = core.App()
 
